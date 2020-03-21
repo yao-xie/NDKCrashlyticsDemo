ad07736e7d6d2bc79ff0472dcce386576baeb975
Yao Xie <yao.xie@hotmail.com>
2020-03-21 23:28:34 +0900

    update force crash test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,27 @@
 #include <jni.h>
 #include <string>
+#include <jni.h>
+#include <stdio.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_xieyao_ndkcrashlyticsdemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++, method #stringFromJNI";
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_xieyao_ndkcrashlyticsdemo_MainActivity_triggerNdkCrash(
+        JNIEnv *env,
+        jobject /* this */) {
+    FILE *file = fopen("/sdcard/hello.txt", "w+");
+//  trigger crash
+//    if (file != NULL) {
+        fputs("HELLO WORLD!\n", file);
+        fflush(file);
+        fclose(file);
+//    }
+    std::string hello = "Hello from C++, method #triggerNdkCrash";
     return env->NewStringUTF(hello.c_str());
 }
--- app/src/main/java/com/xieyao/ndkcrashlyticsdemo/MainActivity.java
+++ app/src/main/java/com/xieyao/ndkcrashlyticsdemo/MainActivity.java
@@ -3,9 +3,17 @@ package com.xieyao.ndkcrashlyticsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -20,6 +28,34 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        findViewById(R.id.btn_java_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Crashlytics.getInstance().crash(); // Force a crash
+            }
+        });
+
+        findViewById(R.id.btn_unsatisfied_link_error).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nonExsitedNativeMethod();
+            }
+        });
+
+        findViewById(R.id.btn_native_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = triggerNdkCrash();
+                Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 
     /**
@@ -27,4 +63,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public native String nonExsitedNativeMethod();
+
+    public native String triggerNdkCrash();
 }
